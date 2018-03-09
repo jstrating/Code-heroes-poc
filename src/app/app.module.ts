5e702bf9fe8d041670f1584c3346661e9c98863f
jstrating <jesper.strating@outlook.com>
2018-03-09 12:32:12 +0100

    Basic routing + dashboard and leaderboard component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { environment } from '../environments/environment';
 
+import { RouterModule, Routes } from '@angular/router';
 //Component imports
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
@@ -13,15 +14,26 @@ import { AngularFireAuth } from 'angularfire2/auth';
 import { AngularFireModule } from 'angularfire2';
 import { AngularFireDatabase, AngularFireDatabaseModule } from 'angularfire2/database';
 import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { LeaderboardsComponent } from './leaderboards/leaderboards.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+const routes: Routes = [
+  {path: '', redirectTo: 'leaderboards', pathMatch: 'full'},
+  {path: 'leaderboards', component: LeaderboardsComponent},
+  {path: 'dashboard', component: LeaderboardsComponent},
+]
 
 @NgModule({
   declarations: [
     AppComponent,
+    LeaderboardsComponent,
+    DashboardComponent,
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     SharedModule,
+    RouterModule.forRoot(routes, {useHash: true}),
     AngularFireModule.initializeApp(environment.firebase, 'code-heroes'),
     AngularFirestoreModule,
     AngularFireDatabaseModule,
