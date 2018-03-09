dbadf8f26358d8e2addb5884cad70adb61ab0100
jstrating <jesper.strating@outlook.com>
2018-03-09 11:45:06 +0100

    Add angularfire2 + init firebase hosting

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { environment } from '../environments/environment';
 
+//Component imports
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+//Angularfire2 + firebase imports
+import { AngularFireAuth } from 'angularfire2/auth';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabase, AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
 
 @NgModule({
   declarations: [
@@ -13,7 +21,11 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    SharedModule
+    SharedModule,
+    AngularFireModule.initializeApp(environment.firebase, 'code-heroes'),
+    AngularFirestoreModule,
+    AngularFireDatabaseModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyA20YR_BvItuW0Xy2IXlEPTPjPjzTQrxrY",
+    authDomain: "m4m-code-heroes.firebaseapp.com",
+    databaseURL: "https://m4m-code-heroes.firebaseio.com",
+    projectId: "m4m-code-heroes",
+    storageBucket: "m4m-code-heroes.appspot.com",
+    messagingSenderId: "567701438863"
+  }
 };
