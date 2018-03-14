77b1d374c029d148ca9ba1aefefeb75739d5b3f5
jstrating <jesper.strating@outlook.com>
2018-03-14 15:26:08 +0100

    Refactor to scss, import foundation 6, add leaderboards basics, alter layout to have sidenav

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,17 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatToolbarModule} from '@angular/material';
+import { MatToolbarModule, MatSidenavModule, MatIconModule, MatButtonModule} from '@angular/material';
 import {MatTabsModule} from '@angular/material/tabs';
 import { WrapperComponent } from './wrapper/wrapper.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { ActivitySideNavComponent } from './activity-side-nav/activity-side-nav.component';
 
 @NgModule({
     imports: [
         CommonModule,
         MatToolbarModule,
-        MatTabsModule
+        MatTabsModule,
+        MatSidenavModule,
+        MatIconModule,
+        MatButtonModule
     ],
-    declarations: [WrapperComponent, NavbarComponent],
+    declarations: [WrapperComponent, NavbarComponent, ActivitySideNavComponent],
     exports: [WrapperComponent, NavbarComponent]
 })
 export class SharedModule {}
