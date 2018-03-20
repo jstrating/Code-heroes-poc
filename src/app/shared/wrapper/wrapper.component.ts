e137e90f17f0eb3fa0549f3044c85dbf2e487bc8
jstrating <jesper.strating@outlook.com>
2018-03-20 09:52:57 +0100

    Change gradient bg, add font, add emptystate

--- src/app/shared/wrapper/wrapper.component.ts
+++ src/app/shared/wrapper/wrapper.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../auth/auth.service';
 
 @Component({
   selector: 'app-wrapper',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WrapperComponent implements OnInit {
 
-  constructor() { }
+  constructor(public auth: AuthService) { }
 
   ngOnInit() {
   }
